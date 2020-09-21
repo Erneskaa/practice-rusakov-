2614b1fbbb938080ca1df067f57f166f534a803b
Erneskaa <erny8262@gmail.com>
2020-09-21 20:15:14 +0300

    append practice 1.3 (mihalich-zver)

---
+++ practice (1.3 Mihalich-zver)/practice (1.3)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practice__1._3_
+{
+    class Program
+    {
+        SByte a = 0;
+        Byte b = 0;
+        Int16 c = 0;
+        Int32 d = 0;
+        Int64 e = 0;
+        string s = "";
+        Exception ex = new Exception();
+   
+    }
+}
