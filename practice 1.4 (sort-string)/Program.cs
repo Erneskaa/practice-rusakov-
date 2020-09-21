914c7f676506a99da092258fdcd609ad4ddc61f7
Erneskaa <erny8262@gmail.com>
2020-09-21 20:26:46 +0300

    append practice 1.4 and changed file practice 1.3

---
+++ practice 1.4 (sort-string)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_1._4__sort_string_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
